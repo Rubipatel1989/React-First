6e9e6da7c390eebc62207ead10849f2e80241b1b
Pawan Kumar <pawank@exeire.com>
2025-06-02 12:04:44 +0530

    Second Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link className="navbar-brand" to="/">MyWebsite</Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/products">Products</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pricing">Pricing</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
